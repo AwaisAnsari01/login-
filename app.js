acd659cf8794ab31a74a722b6a525b661a245f41
AwaisAnsari01 <mransari01234@gmail.com>
2024-08-05 00:49:06 +0500

    updated

--- app.js
+++ app.js
@@ -1,29 +1,50 @@
-import {  signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
+import {signInWithEmailAndPassword , GoogleAuthProvider , signInWithPopup } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 import { auth } from "./config.js";
 
 
+
+
+
+const form = document.querySelector('#form');
+const email = document.querySelector('#email');
+const password = document.querySelector('#password');
+const googleBtn = document.querySelector('#btn')
+
+
+
+
+
+
+form.addEventListener('submit' , (event)=>{
+  event.preventDefault();
  
- const form = document.querySelector('#form');
- const email = document.querySelector('#email');
- const password = document.querySelector('#password');
-
-
-
-
- form.addEventListener('submit', (event)=>{
-  event.preventDefault()
   signInWithEmailAndPassword(auth, email.value, password.value)
   .then((userCredential) => {
     
     const user = userCredential.user;
     console.log(user);
     window.location = 'home.html'
-    
   })
   .catch((error) => {
     const errorCode = error.code;
     const errorMessage = error.message;
     console.log(errorMessage);
   });
+})
 
- })
+const provider = new GoogleAuthProvider();
+
+googleBtn.addEventListener("click", () => {
+  console.log("google login");
+
+  signInWithPopup(auth, provider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+      window.location = "home.html";
+    })
+    .catch((error) => {
+      const errorMessage = error.message;
+      console.log(errorMessage);
+    });
+});
--- home.js
+++ home.js
@@ -1,26 +1,100 @@
-import {  onAuthStateChanged , signOut } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js";
-import { auth } from "./config.js"
+import { onAuthStateChanged, signOut } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+import { auth, db } from "./config.js";
+import { collection, addDoc, getDocs, deleteDoc, doc, updateDoc } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
 
 onAuthStateChanged(auth, (user) => {
     if (user) {
-    
-      const uid = user.uid;
-      console.log(uid);
+        const uid = user.uid;
+        console.log(uid);
     } else {
-    //   window.location = 'index.html'
+        window.location = 'index.html';
     }
-  });
+});
 
+let arr = []
+const logout = document.querySelector('#logout-btn');
 
-  const Logout = document.querySelector("#Logout-btn")
-  Logout.addEventListener('click' , ()=>{
-    signOut(auth)
-    .then(() => {
-        console.log('signed out successfully');
-        window.location = 'index.html'
-      }).catch((error) => {
+logout.addEventListener('click', () => {
+    signOut(auth).then(() => {
+        window.location = 'index.html';
+    }).catch((error) => {
         console.log(error);
-      });
+    });
+});
 
+const form = document.querySelector("#form");
+const todo = document.querySelector("#todo");
+const ul = document.querySelector("#ul");
 
-  })
+async function getData() {
+    const querySnapshot = await getDocs(collection(db, "todos"));
+    arr = []; 
+    querySnapshot.forEach((doc) => {
+        arr.push({ id: doc.id, ...doc.data() }); 
+    });
+    renderTodo();
+}
+
+getData();
+
+function renderTodo() {
+    ul.innerHTML = "";
+    if (arr.length === 0) {
+        ul.innerHTML = "Data not found..";
+        return;
+    }
+    arr.forEach((item) => {
+        ul.innerHTML += `
+            <li class="div-1" id="${item.id}">${item.todo}
+            <button class="deleteBtn">Delete</button>
+            <button class="editBtn">Edit</button>
+            </li> <br>
+        `;
+    });
+
+    const deleteButtons = document.querySelectorAll(".deleteBtn");
+    deleteButtons.forEach((btn) => {
+        btn.addEventListener("click", async (event) => {
+            const li = event.target.parentElement;
+            const id = li.id;
+            await deleteDoc(doc(db, "todos", id));
+            console.log("Data deleted");
+            arr = arr.filter(item => item.id !== id);
+            renderTodo();
+        });
+    });
+
+    const editButtons = document.querySelectorAll(".editBtn");
+    editButtons.forEach((btn) => {
+        btn.addEventListener("click", async (event) => {
+            const li = event.target.parentElement;
+            const id = li.id;
+            const updatedNewValue = prompt("Enter new value");
+            if (updatedNewValue) {
+                const todoUpdate = doc(db, "todos", id);
+                await updateDoc(todoUpdate, { todo: updatedNewValue });
+                console.log("Data updated");
+                const item = arr.find(item => item.id === id);
+                if (item) {
+                    item.todo = updatedNewValue;
+                }
+                renderTodo();
+            }
+        });
+    });
+}
+
+form.addEventListener("submit", async (event) => {
+    event.preventDefault();
+    const todoText = todo.value.trim();
+    if (!todoText) return; 
+    try {
+        const docRef = await addDoc(collection(db, "todos"), { todo: todoText });
+        console.log("Document written with ID: ", docRef.id);
+        arr.push({ id: docRef.id, todo: todoText });
+        renderTodo();
+        todo.value=""
+    } catch (e) {
+        console.error("Error adding document: ", e);
+    }
+});
